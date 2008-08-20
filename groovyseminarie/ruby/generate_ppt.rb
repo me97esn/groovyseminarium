b6c4eb940a2facc37cc4de817e9baaaf2042960c
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-08-20 13:25:11 +0000

    kör program utan parametrar

--- groovyseminarie/ruby/generate_ppt.rb
+++ groovyseminarie/ruby/generate_ppt.rb
@@ -4,6 +4,7 @@ ppt = WIN32OLE.new('Powerpoint.Application')
 ppt.Visible = true
 @pre = ppt.presentations.Open("C:/programmering/workspace/Groovyseminarie/dokument/groovyseminarium_source.pptx")
 
+
 def replaceCodeAnchors
   @pre.Slides.each{|slide|
   slide.Shapes.each{|shape|
@@ -12,10 +13,7 @@ def replaceCodeAnchors
 }
 end
 
-def replace(ole_shape)
-  
-  p ole_shape.ole_methods
-  
+def replace(ole_shape) 
   fileName = ole_shape.TextFrame.TextRange.Text
   fileName["["]=""
   fileName["]"]=""
@@ -23,10 +21,19 @@ def replace(ole_shape)
   fileUrl = "C:/programmering/workspace/groovyseminarie/src/se/britech/groovySeminarie/groovy/#{fileName}"
   text = "#{fileName}\n-------------------------------------------\n"
   File.open(fileUrl, "r").each{|l| text += l }
+  
+  # Sätt texten
   ole_shape.TextFrame.TextRange.Text = text
   ole_shape.TextFrame.TextRange.Font.Name = "Courier NEW";
   ole_shape.TextFrame.TextRange.Font.Size = 15;
   ole_shape.TextFrame.TextRange.ParagraphFormat.Alignment = 1 # left
+  
+  # Skapa en länk
+#  p ole_shape.TextFrame.TextRange.Item(MouseClick)
+
+  onClick = 1
+  ole_shape.TextFrame.TextRange.ActionSettings.Item(onClick).Action = 9
+  ole_shape.TextFrame.TextRange.ActionSettings.Item(onClick).Hyperlink.Address = "C:/programmering/workspace/groovyseminarie/ruby/run_groovy_script.rb"
 end
 
 replaceCodeAnchors

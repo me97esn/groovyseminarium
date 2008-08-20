9ac67674968411db8282a4b3fbbe9313c60175bf
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-08-20 12:38:37 +0000

    skriv innehållet i filen till ppt

--- groovyseminarie/ruby/generate_ppt.rb
+++ groovyseminarie/ruby/generate_ppt.rb
@@ -2,16 +2,37 @@ require 'win32ole'
 
 ppt = WIN32OLE.new('Powerpoint.Application')
 ppt.Visible = true
-pre = ppt.Presentations.Open("C:\\programmering\\workspace\\Groovyseminarie\\dokument\\groovyseminarium_source.pptx")
+@pre = ppt.presentations.Open("C:/programmering/workspace/Groovyseminarie/dokument/groovyseminarium_source.pptx")
 
-pre.Slides.each{|slide|
+def replaceCodeAnchors
+  @pre.Slides.each{|slide|
   slide.Shapes.each{|shape|
-    p shape.TextFrame.TextRange.Text 
+    replace( shape ) if shape.TextFrame.TextRange.Text =~ /\[.*\]/ 
   }
 }
+end
+
+def replace(ole_shape)
+  
+  p ole_shape.ole_methods
+  
+  fileName = ole_shape.TextFrame.TextRange.Text
+  fileName["["]=""
+  fileName["]"]=""
+  fileName += ".groovy"
+  fileUrl = "C:/programmering/workspace/groovyseminarie/src/se/britech/groovySeminarie/groovy/#{fileName}"
+  text = "#{fileName}\n-------------------------------------------\n"
+  File.open(fileUrl, "r").each{|l| text += l }
+  ole_shape.TextFrame.TextRange.Text = text
+  ole_shape.TextFrame.TextRange.Font.Name = "Courier NEW";
+  ole_shape.TextFrame.TextRange.Font.Size = 15;
+  ole_shape.TextFrame.TextRange.ParagraphFormat.Alignment = 1 # left
+end
+
+replaceCodeAnchors
 
 #  Run
-pre.SlideShowSettings.Run
+@pre.SlideShowSettings.Run
 
 # close powerpoint, will close all the currently open files
-ppt.Quit()
+#ppt.Quit()

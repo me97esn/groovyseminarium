51bff6776404010d46d68e426d12b685ccdfc5a6
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-08-22 11:59:42 +0000

    la till DaysAgo

---
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/DaysAgo.groovy
@@ -0,0 +1,13 @@
+package se.britech.groovySeminarie.groovy
+
+Integer.metaClass.propertyMissing ={name-> 
+	if (name == "days") {
+		return delegate * 24 * 60 * 60 * 1000
+	}else if(name == "ago"){
+		def time = new Date().time
+		time -= delegate
+		return new Date(time:time)
+	}
+}
+
+println 4.days.ago 

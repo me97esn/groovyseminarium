8d8d689fa941859ec0955014567334c4b7ade569
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-08-22 11:15:47 +0000

    lagt till unreadable

---
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/Unreadable.groovy
@@ -0,0 +1,12 @@
+package se.britech.groovySeminarie.groovy
+
+// Använd metaClass för att överlagra en metod 
+Integer.metaClass.invokeMethod ={name, args-> 
+	if (name == "plus") {
+		print "$delegate + ${args?.getAt(0)} = "
+		return Integer.metaClass.getMetaMethod("minus",args).invoke(delegate, args)
+	}
+}
+
+// Nu betyder det här 1-1, mao är koden inte längre läslig
+print 1+1

a412f3e9e5a645ce5f877651bfcd2c97d7f58ba0
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-09-02 11:54:07 +0000

    lagt till map conveniance methods

--- groovyseminarie/src/se/britech/groovySeminarie/groovy/MapConveniance.groovy
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/MapConveniance.groovy
@@ -1,2 +1,22 @@
 package se.britech.groovySeminarie.groovy
-map m = ["ett":1, "två":2]
+Map map = ["one":1, "two":2]
+print "map.getClass():"
+println map.getClass()
+println map
+
+print "map.one:"
+println map.one
+
+map.three = 3
+map["four"] = 4
+println map
+
+print 'map.containsKey("one"):'
+println map.containsKey("one")
+
+print 'map.containsValue(2):'
+println map.containsValue(2)
+
+map.each{key, value -> 
+	println key + "=" + value	
+}

c9b3964839abc2b372225d4c02c1b90b3f5dbb26
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-09-15 09:21:14 +0000

    lagt till bild på våg

--- groovyseminarie/src/se/britech/groovySeminarie/groovy/OptionalParameter.groovy
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/OptionalParameter.groovy
@@ -1,6 +1,6 @@
 package se.britech.groovySeminarie.groovy
 def doSomething(String s="Default string", obj=new Object()){
-	println s + obj
+	println "${s}, ${obj}"
 }
 doSomething()
 doSomething("En parameter")

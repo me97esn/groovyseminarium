6685bc920389fc4b48d2ed021e17e0be6f80f32e
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-09-15 08:17:02 +0000

    tagit bort getters/setters från dokument

--- groovyseminarie/src/se/britech/groovySeminarie/groovy/ClassWithGettersSetters.groovy
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/ClassWithGettersSetters.groovy
@@ -1,14 +1,14 @@
 package se.britech.groovySeminarie.groovy
 public class Magazine{
-	def pages
+	private def pages
 	String title
 	def author
 }
 
 def book = new Magazine()
-book.pages = 300
-book.setPages(300)
+book.pages = 200 // Sätt property
+// book.setPages(300) // Setter finns inte till private properties
 
-book.title = "Groovy recipies"
-book.setTitle("Groovy recipies")
+book.title = "Groovy recipies" // Sätt property
+book.setTitle("Groovy recipies") // Setter genereras vid kompilering
 
--- groovyseminarie/src/se/britech/groovySeminarie/groovy/OptionalTyping.groovy
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/OptionalTyping.groovy
@@ -1,10 +1,10 @@
 package se.britech.groovySeminarie.groovy
 String aString = "Jag är en sträng" // Statisk typning
-def aDynamicTypedVariable = "Jag är oxå en sträng" // Dynamisk typning
-aDynamicTypedVariable = 1 
-aDynamicTypedVariable = new HashMap()
+def duckTyped = "Jag är oxå en sträng" // Dynamisk typning
+duckTyped = 1 
+duckTyped = new HashMap()
 
-aDynamicTypedVariable.doSomeThing() // Kompilerar, men kommer smälla när det körs
+duckTyped.doSomeThing() // Kompilerar, men kommer smälla när det körs
 
 void testSomething(){ // jUnit 
 	

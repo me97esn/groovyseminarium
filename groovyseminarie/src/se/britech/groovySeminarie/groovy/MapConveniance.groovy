2545842f3908aa8d7dce874a514300fd5c464d43
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-10-08 15:13:32 +0000

    addera ihop två maps

--- groovyseminarie/src/se/britech/groovySeminarie/groovy/MapConveniance.groovy
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/MapConveniance.groovy
@@ -2,14 +2,16 @@ package se.britech.groovySeminarie.groovy
 LinkedHashMap map0 = new LinkedHashMap()
 map0.put("one", 1)
 map0.put("two", 2)
-
 // Samma sak som:
 LinkedHashMap map = ["one":1, "two":2]
 
 println map.one
 
 map.three = 3
-map["four"] = 4
+// Samma sak som:
+map["three"] = 3
+// Samma sak som :
+map.put("three", 3)
 
 println map.containsKey("one")
 
@@ -18,3 +20,5 @@ println map.containsValue(2)
 map.each{key, value -> 
 	println key + "=" + value	
 }
+
+println map + ["four":4,"five":5]

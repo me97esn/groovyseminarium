db66711faa7ed2b368f59d938107e33a39805493
me97esn <me97esn@e8d006ed-a59e-49b1-a57d-de613967c1e9>
2008-10-08 15:08:33 +0000

    rensat lite i ArrayConveniance

--- groovyseminarie/src/se/britech/groovySeminarie/groovy/ArrayConveniance.groovy
+++ groovyseminarie/src/se/britech/groovySeminarie/groovy/ArrayConveniance.groovy
@@ -16,13 +16,11 @@ println list.reverse()
 
 println list + ["Claremont", "Clarity"]
 
-list.each{ println it }
 list.eachWithIndex{item, i-> 
 	println item + ":" +  i 
 }
 println list.collect{ it.toUpperCase()}
 
-println list.findAll{it =~ /Bri.*/}
 
 
 
